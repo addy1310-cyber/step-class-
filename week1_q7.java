98013abd33df39bc2f605206064a01b36ce28744
as0287@srmist.edu.in <ritzzzbeniwal@gmail.com>
2026-03-11 15:05:50 +0530

    question 6

---
+++ week1_q7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+   
+    double volume_earth=(4.0/3.0)*3.14*Math.pow( 6378, 3);
+    System.out.println("the volume of earth : "+volume_earth);
+  
+}
+    
+
