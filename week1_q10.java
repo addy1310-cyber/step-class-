ac22a11b1514011a0ca2f48cff9d890b8b49521f
as0287@srmist.edu.in <ritzzzbeniwal@gmail.com>
2026-03-12 22:01:14 +0530

    new

---
+++ week1_q10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public static void main(String [] args){
+    Scanner sc = new Scanner (System. in);
+    double ht ;
+    System.out.println("enter ur heitght in cm :");
+    double height_inch= ht*2.54;
+    System.out.println(" The height of the person in inches :"+height_inch +"Height in feet :");
+
+}
---
+++ week1_q9.java
@@ -0,0 +1,14 @@
+
+    
+import java.util.*;
+public static void main (String [] args){
+    Scanner sc = new Scanner (System.in);
+    System.out.println("enter the student fee : ");
+    double stud_fee=sc.nextDouble();
+    System.out.println("Enter  discount percentage : ");
+    int discountPercentage = sc.nextInt();
+     double disc_amt = stud_fee*(discountPercentage/100.0);
+    double final_fee = stud_fee-disc_amt;
+    System.out.println("discount amount in iNR : "+ disc_amt+ "The final fee needed to be paid by the student : "+final_fee);
+
+}
